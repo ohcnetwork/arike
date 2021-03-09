ba58123e94797a39cb3af28e9140c3ad547a6479
Bodhish Thomas <bodhish@gmail.com>
2021-03-10 02:35:31 +0530

    Clear host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,5 +118,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.hosts << "arike.coronasafe.network"
+  config.hosts.clear
 end
