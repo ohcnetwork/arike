eec943d8232856082fe65b1ad7a27c9d28c6fc3b
Jasim A Basheer <jasim.ab@gmail.com>
2021-03-15 13:41:22 +0530

    Add new patient

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,4 +1,11 @@
 class PatientsController < ApplicationController
   def new
+    @patient = Patient.new
+  end
+
+  def create
+    patient = params.require(:patient).permit(:full_name, :first_name)
+    Patient.create!(patient)
+    redirect_to root_path
   end
 end
