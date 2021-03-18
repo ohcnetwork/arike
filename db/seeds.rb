8ba01254f106f98ff2dd0bbe8b41e9311566349b
Peeyush <peeyushgoyal154@gmail.com>
2021-03-18 14:12:33 +0530

    Fix: Patients View table.
    Co-authored-by: Abhinandan Arya <abhinandanarya06122000@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,37 +6,37 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!([
-  { first_name: "Pulkit", full_name: "Pulkit Ahuja", role: "asha", password: "0" },
-  { first_name: "Yash", full_name: "Yash Gupta", role: "asha", password: "0" },
-  { first_name: "Rajesh", full_name: "Rajesh Sharma", role: "asha", password: "0" },
-  { first_name: "Himanshu", full_name: "Himanshu", role: "asha", password: "0" },
-  { first_name: "Abhay", full_name: "Abhay Ahuja", role: "asha", password: "0" },
-])
+# User.create!([
+#   { first_name: "Pulkit", full_name: "Pulkit Ahuja", role: "asha", password: "0" },
+#   { first_name: "Yash", full_name: "Yash Gupta", role: "asha", password: "0" },
+#   { first_name: "Rajesh", full_name: "Rajesh Sharma", role: "asha", password: "0" },
+#   { first_name: "Himanshu", full_name: "Himanshu", role: "asha", password: "0" },
+#   { first_name: "Abhay", full_name: "Abhay Ahuja", role: "asha", password: "0" },
+# ])
 
-User.create!([
-  { first_name: "Abhinandan", full_name: "Abhinandan Arya", role: "volunteer", password: "0" },
-  { first_name: "Tanishq", full_name: "Tanishq Agarwal", role: "volunteer", password: "0" },
-  { first_name: "Aman", full_name: "Aman Gupta", role: "volunteer", password: "0" },
-  { first_name: "Naveen", full_name: "Naveen Rohilla", role: "volunteer", password: "0" },
-  { first_name: "Raj", full_name: "Raj Mishra", role: "volunteer", password: "0" },
-])
+# User.create!([
+#   { first_name: "Abhinandan", full_name: "Abhinandan Arya", role: "volunteer", password: "0" },
+#   { first_name: "Tanishq", full_name: "Tanishq Agarwal", role: "volunteer", password: "0" },
+#   { first_name: "Aman", full_name: "Aman Gupta", role: "volunteer", password: "0" },
+#   { first_name: "Naveen", full_name: "Naveen Rohilla", role: "volunteer", password: "0" },
+#   { first_name: "Raj", full_name: "Raj Mishra", role: "volunteer", password: "0" },
+# ])
 
-User.create!([
-  { first_name: "Kajal", full_name: "Kajal Brar", role: "primary_nurse", password: "0" },
-  { first_name: "Ansh", full_name: "Ansh Aggarwal", role: "primary_nurse", password: "0" },
-  { first_name: "Darshana", full_name: "Darshana Thakkar", role: "primary_nurse", password: "0" },
-  { first_name: "Kalyani", full_name: "Kalyani Nayak", role: "primary_nurse", password: "0" },
-  { first_name: "Dhruv", full_name: "Dhruv Pathak", role: "primary_nurse", password: "0" },
-])
+# User.create!([
+#   { first_name: "Kajal", full_name: "Kajal Brar", role: "primary_nurse", password: "0" },
+#   { first_name: "Ansh", full_name: "Ansh Aggarwal", role: "primary_nurse", password: "0" },
+#   { first_name: "Darshana", full_name: "Darshana Thakkar", role: "primary_nurse", password: "0" },
+#   { first_name: "Kalyani", full_name: "Kalyani Nayak", role: "primary_nurse", password: "0" },
+#   { first_name: "Dhruv", full_name: "Dhruv Pathak", role: "primary_nurse", password: "0" },
+# ])
 
-User.create!([
-  { first_name: "Pulkit", full_name: "Tara Vaidya", role: "secondary_nurse", password: "0" },
-  { first_name: "Nilam", full_name: "Nilam Natarajan", role: "secondary_nurse", password: "0" },
-  { first_name: "Jivika", full_name: "Jivika Malhotra", role: "secondary_nurse", password: "0" },
-  { first_name: "Sudhir", full_name: "Sudhir Kashyap", role: "secondary_nurse", password: "0" },
-  { first_name: "Kabir", full_name: "Kabir Munshi", role: "secondary_nurse", password: "0" },
-])
+# User.create!([
+#   { first_name: "Pulkit", full_name: "Tara Vaidya", role: "secondary_nurse", password: "0" },
+#   { first_name: "Nilam", full_name: "Nilam Natarajan", role: "secondary_nurse", password: "0" },
+#   { first_name: "Jivika", full_name: "Jivika Malhotra", role: "secondary_nurse", password: "0" },
+#   { first_name: "Sudhir", full_name: "Sudhir Kashyap", role: "secondary_nurse", password: "0" },
+#   { first_name: "Kabir", full_name: "Kabir Munshi", role: "secondary_nurse", password: "0" },
+# ])
 
 # Panchayat/Municipliaty/Corporation
 kinds = ["Panchayat", "Municipliaty", "Corporation"]
