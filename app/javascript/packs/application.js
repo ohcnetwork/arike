38e63c72e2808013bfc6ecebaf6582af2cb31ffe
ProCode2 <bpro249@gmail.com>
2021-03-19 10:01:35 +0530

    Fix: A miltiple rendering issue with the autocomplete partial

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
-import "./autocomplete";
 
 Rails.start();
 Turbolinks.start();
