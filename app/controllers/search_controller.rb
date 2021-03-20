c5113537e1c77c503f19d4e9dc047af7464bc336
Jasim A Basheer <jasim.ab@gmail.com>
2021-03-20 16:37:44 +0530

    Remove connection switching

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,23 +4,7 @@ class SearchController < ApplicationController
   # You can implement pattern searching using LIKE clause
   # Example : SELECT * FROM patients WHERE first_name LIKE "%pattern%";
 
-  def with_read_only_connection
-    original_connection = ActiveRecord::Base.remove_connection
-    ActiveRecord::Base.establish_connection(:development)
-    ActiveRecord::Base.connection.execute("set session characteristics as transaction read only;")
-    yield
-  ensure
-    ActiveRecord::Base.establish_connection(original_connection)
-  end
-
   def index
-    # query = params[:query]
-    # res = with_read_only_connection do
-    #   ActiveRecord::Base.connection.execute("#{query}")
-    # end
-    # render json: res.to_json
-
-    # sending asha members info to AutoComplete react component
     result = User.where(role: "asha").select(:id, :full_name)
     data = result.map { |r| r.attributes.symbolize_keys }
     render :json => data
