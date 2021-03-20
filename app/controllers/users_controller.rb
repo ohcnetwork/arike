bf5bff14b431e404de9acce97e9a20be8541f906
Mr-DareDevil <arnabsen1729@gmail.com>
2021-03-20 06:41:28 +0530

    Removed unnecessary flash messages and added Sign Up links to navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,11 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-    flash[:notice] = "Added user to system!"
   end
 
   def signup
     @user = User.new
-    @user[:verified]=false
+    @user[:verified] = false
   end
 
   def edit
@@ -32,13 +31,13 @@ class UsersController < ApplicationController
       user[:password] = "arike"
     end
 
-    user= User.new(user)
+    user = User.new(user)
 
     if user.valid?
       user.save
-      redirect_to home_path,notice: 'You have successfully signed up!'
+      redirect_to home_path, notice: "You have successfully signed up!"
     else
-      flash[:error]=user.errors.full_messages.to_sentence
+      flash[:error] = user.errors.full_messages.to_sentence
       redirect_to signup_path
     end
   end
