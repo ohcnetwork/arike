de7da1c6cdfe231cf73d0d4ef9043734582a0bb2
Karan Sanjeev <yo@alphaman.me>
2021-03-20 18:32:17 +0530

    Create Ward page done
    Co-Authored-By: Deepankar Bhade <statisticsaffinity@gmail.com>

--- app/controllers/wards_controller.rb
+++ app/controllers/wards_controller.rb
@@ -4,10 +4,17 @@ class WardsController < ApplicationController
   end
 
   def new
+    @ward = Ward.new
   end
 
   def show
     @ward = Ward.find(params[:id])
+  end
+
+  def create
+    p = params[:ward]
+    ward = LsgBody.find(p[:lsg_body]).wards.create!(name: p[:name], number: p[:number])
+    redirect_to ward_path(ward.id)
   end
 
   def edit
