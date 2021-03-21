c7604a08e7b2181db2d2a594f49a65ee73709bb4
Arnab Sen <arnabsen1729@gmail.com>
2021-03-21 19:00:27 +0530

    Added test for editing data by super user

--- spec/requests/super_user_request_spec.rb
+++ spec/requests/super_user_request_spec.rb
@@ -8,8 +8,26 @@ RSpec.describe "Users", type: :request do
 
   it "verify access of a signed up user by super user" do
     user = FactoryBot.create(:user, verified: false)
-    session_params = { current_user_id: @superuser.id }
     put "/users/#{user.id}/verify"
     expect(User.find_by_id(user.id).verified).to eq(true)
+  end
+
+  it "verify edit of user details by super user" do
+    user = FactoryBot.create(:user, verified: true)
+
+    # making a deep copy
+    oldUser = Marshal.load(Marshal.dump(user))
+    user.full_name = Faker::Name.name
+    user.first_name = Faker::Name.first_name
+    user.email = Faker::Internet.email
+    user.phone = Faker::Number.number(digits: 10)
+    put "/users/#{user.id}", params: { user: { first_name: user.first_name, full_name: user.full_name, role: user.role, email: user.email, phone: user.phone, verified: true } }
+    finalUser = User.find_by_id(user.id)
+    expect(finalUser.full_name).to eq(user.full_name)
+    expect(finalUser.full_name).not_to eq(oldUser.full_name)
+    expect(finalUser.first_name).to eq(user.first_name)
+    expect(finalUser.first_name).not_to eq(oldUser.first_name)
+    expect(finalUser.email).to eq(user.email)
+    expect(finalUser.phone).not_to eq(oldUser.phone)
   end
 end
