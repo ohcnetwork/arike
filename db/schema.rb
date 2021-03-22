6c6c2ba67ec5af59118d38e65546d3eb4caf4815
Amit <amitkr0819@gmail.com>
2021-03-22 14:53:14 +0530

    added necessary fields in visits table
    Co-authored-by: khavinshankar <khavinshankar@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_20_121455) do
+ActiveRecord::Schema.define(version: 2021_03_22_091445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -71,6 +71,9 @@ ActiveRecord::Schema.define(version: 2021_03_20_121455) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "first_visit"
+    t.datetime "last_visit"
+    t.json "records"
     t.index ["patient_id"], name: "index_visits_on_patient_id"
     t.index ["user_id"], name: "index_visits_on_user_id"
   end
