39959bfdbc167a7303d39786940bc6754177704e
developedbysj <developedbysj@gmail.com>
2021-03-24 21:28:19 +0530

    Configure SMTP settings
    Co-authored-by: Shri Hari L <shrihari689@users.noreply.github.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,14 @@ Rails.application.configure do
 
   config.hosts.clear
   config.action_mailer.default_url_options = { :host => "localhost:" + ENV["PORT"] }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV["SMTP_USERNAME"],
+    :password => ENV["SMTP_PASSWORD"],
+    :address => ENV["SMTP_ADDRESS"],
+    :domain => ENV["SMTP_DOMAIN"],
+    :port => ENV["SMTP_PORT"],
+    :authentication => ENV["SMTP_AUTHENTICATION"],
+  }
 end
