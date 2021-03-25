b8644cda4b252918553f51157a1815c2203e4a01
Devansh Kaushik <devanshkaushik0711@gmail.com>
2021-03-25 23:21:58 +0530

    Refactor: Dashboard Presenter

--- app/presenters/dashboard/index_presenter.rb
+++ app/presenters/dashboard/index_presenter.rb
@@ -2,38 +2,32 @@ module Dashboard
   class IndexPresenter < ::ApplicationPresenter
 
     def links
-      schedule + visit + lsg + enroll_patients + calendar+new_users+account_settings+facilities
+      schedule + visit + enroll_patients + calendar + new_users + account_settings + facilities + lsg_body
     end
 
     def link(key, path)
       default_key = "presenters.dashboard.index_presenter.links.#{key}"
-     { title: I18n.t("#{default_key}.title"), text: I18n.t("#{default_key}.text"), icon: I18n.t("#{default_key}.icon"), link: path }
+      { title: I18n.t("#{default_key}.title"), text: I18n.t("#{default_key}.text"), icon: I18n.t("#{default_key}.icon"), link: path }
     end
 
     def schedule
       [link("schedule", view.schedule_path)]
-
     end
 
     def visit
-      [link("visit", view.schedule_path)]
+      [link("visit", "")]
     end
 
     def enroll_patients
       [link("enroll_patients", view.new_patient_path)]
     end
 
-    def lsg
-      return [] unless current_user.role.in? [User.roles[:asha]]
-      [link("lsg", view.lsg_bodies_path)]
-    end
-
     def calendar
-      # return [] unless current_user.role.in? [User.roles[:asha]]
       [link("calendar", view.schedule_path(anchor: "calender"))]
     end
 
     def new_users
+      return [] unless current_user.role.in? [User.roles[:superuser]]
       [link("new_users", view.new_user_path)]
     end
 
@@ -43,6 +37,11 @@ module Dashboard
 
     def facilities
       [link("facilities", view.facilities_path)]
+    end
+
+    def lsg_body
+      return [] unless current_user.role.in? [User.roles[:superuser]]
+      [link("lsg_body", view.lsg_bodies_path)]
     end
   end
 end
