bc432eff0ca6a3fc36b12bdd373e04be8e55d066
Sandra <sandrakakkarayil01@gmail.com>
2021-03-26 13:50:22 +0530

    Redirecting to the different facility pages for different roles
    Co-authored-by: Devansh Kaushik <devanshkaushik0711@gmail.com>

--- app/presenters/dashboard/index_presenter.rb
+++ app/presenters/dashboard/index_presenter.rb
@@ -2,7 +2,7 @@ module Dashboard
   class IndexPresenter < ::ApplicationPresenter
 
     def links
-      schedule + visit + lsg + enroll_patients + calendar+new_users+account_settings+facilities
+      schedule + visit + lsg + enroll_patients + patients + calendar + new_users + users + facilities + account_settings
     end
 
     def link(key, path)
@@ -23,9 +23,8 @@ module Dashboard
       [link("enroll_patients", view.new_patient_path)]
     end
 
-    def lsg
-      return [] unless current_user.role.in? [User.roles[:asha]]
-      [link("lsg", view.lsg_bodies_path)]
+    def patients
+      [link("patients", view.patients_path)]
     end
 
     def calendar
@@ -33,16 +32,37 @@ module Dashboard
       [link("calendar", view.schedule_path(anchor: "calender"))]
     end
 
+    def lsg
+      return [] unless current_user.role.in? [User.roles[:asha]]
+      [link("lsg", view.lsg_bodies_path)]
+    end
+
     def new_users
+      return [] unless current_user.superuser?
       [link("new_users", view.new_user_path)]
     end
 
+    def users
+      return [] unless current_user.superuser?
+      [link("users", view.users_path)]
+    end
+
     def account_settings
-      [link("account_settings", "")]
+      [link("account_settings", view.password_reset_page_path)]
     end
 
     def facilities
-      [link("facilities", view.facilities_path)]
+      if current_user.superuser?
+        [link("facilities", view.facilities_path)]
+      else
+        id = current_user.facility.id
+        if id
+          [link("facilities", view.facility_path(id))]
+        else
+          []
+        end
+      end
     end
+
   end
 end
