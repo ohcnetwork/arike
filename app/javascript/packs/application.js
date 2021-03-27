1b0c8a39e4d60d1f370a6cc59d7998071c8ca495
Arnab Sen <arnabsen1729@gmail.com>
2021-03-27 12:22:22 +0530

    Font awesome integrated and some svgs replaced

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,16 +3,17 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs";
-import Turbolinks from "turbolinks";
-import * as ActiveStorage from "@rails/activestorage";
-import "channels";
-import * as PatientFormPage from "../Patient/New/PersonalDetails/PersonalDetails.bs";
+import Rails from '@rails/ujs';
+import Turbolinks from 'turbolinks';
+import * as ActiveStorage from '@rails/activestorage';
+import 'channels';
+import * as PatientFormPage from '../Patient/New/PersonalDetails/PersonalDetails.bs';
+import '@fortawesome/fontawesome-free/js/all';
 
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
 
-import "stylesheets/application";
+import 'stylesheets/application';
 
 window.PatientFormPage = PatientFormPage;
