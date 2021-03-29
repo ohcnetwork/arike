5c3acfa50bf486235421fe4a2f089691fcefc931
khavinshankar <khavinshankar@gmail.com>
2021-03-29 21:10:34 +0530

    dynamic date and day in calender

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,3 +1,19 @@
 class Visit < ApplicationRecord
   belongs_to :patient
+
+  def self.working_day(inc, initial_date=Date.today)
+    cwday = initial_date.cwday
+    if cwday == 7
+      inc = inc + 1
+    elsif cwday == 6
+      inc = inc + 2
+    else
+      cwday = cwday + inc
+      if cwday == 7 || cwday == 6
+        inc = inc + 2
+      end
+    end
+
+    initial_date+inc
+  end
 end
