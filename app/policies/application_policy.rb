c823c44b475c24c5526d542a6a0e9e8c6367a9d6
ProCode2 <bpro249@gmail.com>
2021-04-06 19:54:36 +0530

    Upd: removed nilclass policy

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -47,18 +47,3 @@ class ApplicationPolicy
     end
   end
 end
-
-class NilClassPolicy < ApplicationPolicy
-  class Scope < Scope
-    def resolve
-      raise Pundit::NotDefinedError, "Cannot scope NilClass"
-    end
-  end
-
-  def show?
-    false
-  end
-
-  alias show_patients? show?
-  alias show_users? show?
-end
