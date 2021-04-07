d60f8915c5c17ef72aa2f1aa08224d38cd5388e7
Abhinandan Arya <abhinandanarya06122000@gmail.com>
2021-04-08 01:26:30 +0530

    fixing rails spec problem

--- spec/factories/familyDetail_factory.rb
+++
@@ -1,6 +0,0 @@
-factory :family_details, class: "FamilyDetail" do
-  full_name { generate(:full_name) }
-  phone { Faker::Number.number(digits: 10) }
-  relation { "Brother" }
-  address { generate(:address) }
-end
--- spec/factories/ward_factory.rb
+++ spec/factories/ward_factory.rb
@@ -1,5 +1,7 @@
-factory :ward, class: "Ward" do
-  name { generate(:name) }
-  number { Faker::Number.number(digits: 2) }
-  lsg_body_id { LsgBody.last.id }
+FactoryBot.define do
+  factory :ward, class: "Ward" do
+    name { generate(:name) }
+    number { Faker::Number.number(digits: 2) }
+    lsg_body_id { LsgBody.last.id }
+  end
 end
