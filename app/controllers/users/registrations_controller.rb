c480fc0b03fe8e90e6ecb223b91733ac812aab56
Karan Sanjeev <yo@alphaman.me>
2021-04-10 16:28:26 +0530

    Clean up registration controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -12,15 +11,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
     render "users/signup", layout: "public"
   end
 
-  # POST /resource
-  def create
+  # GET /resource/edit
+  def edit
     super
   end
-
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
 
   # PUT /resource
   def update
@@ -28,25 +22,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
+  def destroy
+    super
+  end
 
   protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :full_name, :role, :phone])
-  end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
@@ -57,9 +37,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     redirect_to dashboard_path
   end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 end
