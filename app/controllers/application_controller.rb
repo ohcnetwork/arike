4f75233a55eb39784057fae994e42d40b4df61d5
Karan Sanjeev <yo@alphaman.me>
2021-04-10 16:57:17 +0530

    Remove useless auth code in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   before_action :authenticate_user!
-  helper_method :current_user
 
   def ensure_superuser
     redirect_to root_path unless current_user && current_user.superuser?
@@ -12,16 +11,6 @@ class ApplicationController < ActionController::Base
   def ensure_facility_access
     unless current_user && current_user.facility_access?
       redirect_to root_path
-    end
-  end
-
-  def current_user
-    return @current_user if @current_user
-    current_user_id = session[:current_user_id]
-    if current_user_id
-      @current_user = User.find(current_user_id)
-    else
-      nil
     end
   end
 
