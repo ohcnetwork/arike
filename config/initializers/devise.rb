2ec79e5aac234213efdb8c12ffa192e78ea886c3
Karan Sanjeev <yo@alphaman.me>
2021-04-11 18:05:10 +0530

    Fix PR check for secrets

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,13 +9,6 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  # The secret key used by Devise. Devise uses this key to generate
-  # random tokens. Changing this key will render invalid all existing
-  # confirmation, reset password and unlock tokens in the database.
-  # Devise will use the `secret_key_base` as its `secret_key`
-  # by default. You can change it below and use your own secret key.
-  # config.secret_key = '8a03315edf7994c5c326276cb564dfcc610474f380367b47c280ad783dc6901dcc92d520067b5b49058be64e32e3117d9dc32d20b75c08294b90b0fbca51f115'
-
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
