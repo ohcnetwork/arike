29c8acbdb9573e43cbacde5cbde13ee4cac0c2a4
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-04-11 22:41:05 +0530

    🎨 Add Error Messages to UI
    Co-authored-by: Karan Sanjeev <yo@alphaman.me>
    Co-authored-by: Shaliesh <anaandshailu@gmail.com>

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,7 +7,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :full_name, :role, :phone])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :full_name, :role, :phone, :email])
   end
 
   # The path used after sign up.
