a41b8705861aa287517ffe3d239cda7ebef871dd
Karan Sanjeev <yo@alphaman.me>
2021-04-16 21:12:36 +0530

    Allow CSRF token to pass through

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -7,7 +7,7 @@ class Users::SessionsController < Devise::SessionsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:login_id, :password])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:login_id, :password, :authenticity_token])
   end
 
   def after_sign_in_path_for(_resource)
