0ed42599d80b74e69990f084d15d7b8195320462
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-04-16 23:00:36 +0530

    💪 Handle Sign In for Unverified user
    Co-authored-by: Karan Sanjeev <yo@alphaman.me>
    Co-authored-by: Shaliesh <anaandshailu@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,18 @@
 class ApplicationController < ActionController::Base
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-  
+
   protect_from_forgery
   before_action :authenticate_user!
   before_action :ensure_verified_user!
 
-
   def ensure_verified_user!
-    unless current_user && current_user.verified
-      flash[:error] = "Your account is not verified!"
-      sign_out current_user
+    if user_signed_in?
+      unless current_user.verified
+        flash[:error] = "Your account is not verified!"
+        sign_out current_user
+        redirect_to root_path
+      end
     end
   end
 
