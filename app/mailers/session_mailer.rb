dc81b8c295a0f506649e6c868adad60245d5b6d3
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-04-18 10:22:46 +0530

    🧹 Remove Unused Mailer #52

--- app/mailers/session_mailer.rb
+++
@@ -1,8 +0,0 @@
-class SessionMailer < ApplicationMailer
-  def password_reset
-    mail to: params[:email]
-  end
-
-  # Other Emails regarding Sessions like Welcome Email, Verification Email goes here
-
-end
