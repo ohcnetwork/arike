061f726d0573d231c9e5585dd43ccac1ba1c53ff
Apurva <apurvanagar@gmail.com>
2021-04-19 17:43:56 +0530

    updated users authorization to use pundit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,16 @@
 class UsersController < ApplicationController
-  before_action :ensure_superuser, only: %i[update verify]
-  before_action :ensure_facility_access, only: %i[index new]
-
-  def index; end
+  def index
+    authorize User
+  end
 
   def new
     @user = User.new
+    authorize @user
   end
 
   def edit
     @user = User.find(params[:id])
+    authorize @user
   end
 
   def update
@@ -18,6 +19,7 @@ class UsersController < ApplicationController
         .require(:user)
         .permit(:full_name, :first_name, :role, :email, :phone)
     user = User.find(params[:id])
+    authorize user
     if user
       user.update(
         full_name: new_user[:full_name],
@@ -31,15 +33,24 @@ class UsersController < ApplicationController
   end
 
   def create_custom
-    data = params.require(:user).permit(
-      :full_name, :first_name, :role,
-      :email, :password, :phone, :verified
-    )
+    data =
+      params
+        .require(:user)
+        .permit(
+          :full_name,
+          :first_name,
+          :role,
+          :email,
+          :password,
+          :phone,
+          :verified,
+        )
 
     user = User.new(data)
 
     if user.save
-      flash[:notice] = "Created user #{data[:full_name]} successfully with role #{data[:role]}"
+      flash[:notice] =
+        "Created user #{data[:full_name]} successfully with role #{data[:role]}"
     else
       flash[:error] = user.errors.full_messages.to_sentence
     end
@@ -55,7 +66,8 @@ class UsersController < ApplicationController
     user =
       User.add_to_facility(assignables[:user_id], assignables[:facility_id])
     if user.save
-      flash[:success] = "Successfully assigned #{user.full_name} to this facility!"
+      flash[:success] =
+        "Successfully assigned #{user.full_name} to this facility!"
       redirect_to show_facility_users_path(assignables[:facility_id])
     else
       flash[:error] = user.errors.full_messages.to_sentence
@@ -74,7 +86,8 @@ class UsersController < ApplicationController
         assignables[:facility_id],
       )
     if user.save
-      flash[:success] = "Successfully removed #{user.full_name} to this facility!"
+      flash[:success] =
+        "Successfully removed #{user.full_name} to this facility!"
       redirect_to show_facility_users_path(assignables[:facility_id])
     else
       flash[:error] = user.errors.full_messages.to_sentence
@@ -84,6 +97,7 @@ class UsersController < ApplicationController
 
   def verify
     user = User.find_by(id: params[:id])
+    authorize user
     user.update(verified: true) if user
     redirect_to users_path
   end
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,14 @@
 class UserPolicy < ApplicationPolicy
+  def index?
+    user && user.facility_access?
+  end
+
+  alias new? index?
+
+  def edit?
+    user && user.superuser?
+  end
+
+  alias update? edit?
+  alias verify? edit?
 end
