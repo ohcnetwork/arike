143a02e659ef88f1cd0be853e2520184b39025af
Apurva <apurvanagar@gmail.com>
2021-04-19 21:41:18 +0530

    only superuser can see users on the platform

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,14 +1,10 @@
 class UserPolicy < ApplicationPolicy
   def index?
-    user && user.facility_access?
-  end
-
-  alias new? index?
-
-  def edit?
     user && user.superuser?
   end
 
-  alias update? edit?
-  alias verify? edit?
+  alias new? index?
+  alias edit? index?
+  alias update? index?
+  alias verify? index?
 end
