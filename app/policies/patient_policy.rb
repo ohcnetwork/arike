9391e514c77d0bdc69a0a0f1dc60c80981013788
Apurva Nagar <apurvanagar@gmail.com>
2021-04-20 17:50:09 +0530

    refactored patient policy scope.
    Co-authored-by: Pradipta Ghoshal <50803990+ProCode2@users.noreply.github.com>

--- app/policies/patient_policy.rb
+++ app/policies/patient_policy.rb
@@ -20,8 +20,7 @@ class PatientPolicy < ApplicationPolicy
         Facility
           .where(id: user.facility.id)
           .or(Facility.where(parent_id: user.facility.id))
-      if user.secondary_nurse? || user.medical_officer?
-        return Patient.joins(:facility).where(facilities: all_facilities)
+return Patient.joins(:facility).where(facilities: all_facilities) if user.secondary_nurse? || user.medical_officer?
       end
 
       return user.facility.patients if user.primary_nurse?
