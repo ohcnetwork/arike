0a68ff92168897eec9564c843c08d2c2391fda5b
Peeyush <peeyushgoyal154@gmail.com>
2021-08-10 23:43:18 +0530

    Removed unexpected end

--- app/policies/patient_policy.rb
+++ app/policies/patient_policy.rb
@@ -21,7 +21,6 @@ class PatientPolicy < ApplicationPolicy
           .where(id: user.facility.id)
           .or(Facility.where(parent_id: user.facility.id))
 return Patient.joins(:facility).where(facilities: all_facilities) if user.secondary_nurse? || user.medical_officer?
-      end
 
       return user.facility.patients if user.primary_nurse?
 
