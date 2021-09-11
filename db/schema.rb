52843c2604e82e3399b06460f26fb2bff9ee92de
Sandra <sandrakakkarayil01@gmail.com>
2021-09-11 18:26:44 +0530

    Rerum migrations

--- db/schema.rb
+++ db/schema.rb
@@ -64,14 +64,6 @@ ActiveRecord::Schema.define(version: 2021_08_20_131426) do
     t.string "remarks"
   end
 
-  create_table "general_health_informations", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "visit_id"
-    t.string "akps"
-    t.string "palliative_phase"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "lsg_bodies", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name"
     t.string "kind"
@@ -121,63 +113,15 @@ ActiveRecord::Schema.define(version: 2021_08_20_131426) do
     t.string "patient_views"
     t.string "family_views"
     t.datetime "expired"
+    t.json "treatment"
     t.string "state"
     t.string "district"
-    t.json "treatment"
     t.index ["facility_id"], name: "index_patients_on_facility_id"
   end
 
   create_table "patients_users", force: :cascade do |t|
     t.uuid "patient_id"
     t.uuid "user_id"
-  end
-
-  create_table "physical_examinations", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "visit_id"
-    t.string "bp"
-    t.string "grbs"
-    t.string "rr"
-    t.string "pulse"
-    t.string "personal_hygiene"
-    t.string "mouth_hygiene"
-    t.string "pubic_hygiene"
-    t.string "systemic_examination"
-    t.string "systemic_examination_details"
-    t.uuid "done_by"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "physical_symptoms", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "visit_id"
-    t.string "patient_at_peace"
-    t.string "pain"
-    t.string "shortness_breath"
-    t.string "weakness"
-    t.string "poor_mobility"
-    t.string "nausea"
-    t.string "vomiting"
-    t.string "poor_appetite"
-    t.string "constipation"
-    t.string "sore"
-    t.string "drowsiness"
-    t.string "wound"
-    t.string "lack_of_sleep"
-    t.string "micnutrition"
-    t.string "other_symptoms"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "psychological_reviews", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "visit_id"
-    t.string "patient_worried"
-    t.string "family_anxious"
-    t.string "patient_depressed"
-    t.string "patient_feels"
-    t.string "patient_informed"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "states", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -203,7 +147,7 @@ ActiveRecord::Schema.define(version: 2021_08_20_131426) do
     t.bigint "phone"
     t.boolean "verified", default: false
     t.uuid "facility_id"
-    t.string "encrypted_password", default: ""
+    t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -253,10 +197,6 @@ ActiveRecord::Schema.define(version: 2021_08_20_131426) do
     t.boolean "assigned_to_primary_nurse", default: false
     t.boolean "assigned_to_physiotherapist", default: false
     t.boolean "is_doctor_accompanying", default: false
-    t.uuid "general_health_information_id"
-    t.uuid "psychological_review_id"
-    t.uuid "physical_symptoms_id"
-    t.uuid "physical_examination_id"
   end
 
   create_table "visits", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
