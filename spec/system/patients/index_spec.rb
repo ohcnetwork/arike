6b23e48a2d168248a12ee56aa7d4badd025bc939
Peeyush <peeyushgoyal154@gmail.com>
2021-09-30 18:27:03 +0530

    Editing Personal Details

--- spec/system/patients/index_spec.rb
+++ spec/system/patients/index_spec.rb
@@ -40,5 +40,53 @@ feature 'Index spec' do
       expect(page).to have_text(@patient.full_name)
 
     end
+    scenario 'Edit Name of Patient' do
+      @user = FactoryBot.create(:user, role: User.roles[:superuser], verified: true)
+      login_as(@user)
+
+      @patient = Patient.last
+      within(find("div.desktopLayoutSidebar")) do
+        click_on("Patients")
+      end
+
+      click_on("View")
+      click_on("Edit Personal Details")
+
+      name = generate(:full_name)
+      dob = Faker::Date.in_date_period
+      gender = ["Male", "Female", "Other"].sample
+      phone = Faker::Number.number(digits: 10)
+      emergency = Faker::Number.number(digits: 10)
+      facility = Facility.last.name
+      route = Faker::Address.full_address
+      address = Faker::Address.full_address
+      notes = Faker::Lorem.paragraph
+      volunteer = User.where(role: "Volunteer").last
+      economic_status = ["Well Off", "Middle Class", "Poor", "Very Poor"].sample
+
+      fill_in "patient[full_name]", with: name
+      fill_in "patient[dob]", with: dob
+      select gender, from: "patient[gender]"
+      fill_in "patient[phone]", with: phone
+      fill_in "patient[emergency_phone_no]", with: emergency
+      select facility, from: "patient[facility_id]"
+      fill_in "patient[route]", with: route
+      fill_in "patient[address]", with: address
+      fill_in "patient[notes]", with: notes
+      # find("label[name='patient[volunteer[#{volunteer.id}]]']").click
+      select economic_status, from: "patient[economic_status]"
+
+      within(find("div.actions")) do
+        click_on("Submit")
+      end
+
+      click_on("Personal Details")
+
+      expect(page).to have_text("Patients")
+      expect(page).to have_text(name)
+      expect(page).to have_text(dob)
+      expect(page).to have_text(phone)
+      expect(page).to have_text(address)
+    end
   end
 end
