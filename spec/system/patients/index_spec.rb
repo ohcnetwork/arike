dbc75d6cd0139759c7a4df619d13235f6644bef2
Peeyush <peeyushgoyal154@gmail.com>
2021-09-30 18:18:52 +0530

    Viewing Patient

--- spec/system/patients/index_spec.rb
+++ spec/system/patients/index_spec.rb
@@ -3,9 +3,22 @@ require 'rails_helper'
 feature 'Index spec' do
   include UserSpecHelper
 
+  before(:each) do
+    FactoryBot.create(:state)
+    FactoryBot.create(:district)
+    FactoryBot.create(:lsg_body)
+    FactoryBot.create(:ward)
+    FactoryBot.create(:facility, kind: 'CHC')
+    FactoryBot.create(:patient)
+    (1...5).each do
+      FactoryBot.create(:volunteer)
+    end
+  end
+
   context 'Superuser', js: true do
     scenario 'View Patients' do
-      login_as("admin@arike.com", "check12345")
+      @user = FactoryBot.create(:user, role: User.roles[:superuser], verified: true)
+      login_as(@user)
 
       within(find("div.desktopLayoutSidebar")) do
         click_on("Patients")
@@ -14,17 +27,18 @@ feature 'Index spec' do
       expect(page).to have_text('Patients')
     end
 
-    # scenario 'View Patient Information' do
-    #   login_as("admin@arike.com", "check12345")
+    scenario 'View Patient Information' do
+      @user = FactoryBot.create(:user, role: User.roles[:superuser], verified: true)
+      login_as(@user)
 
-    #   within(find("div.desktopLayoutSidebar")) do
-    #     click_on("Patients")
-    #   end
+      @patient = Patient.last
+      within(find("div.desktopLayoutSidebar")) do
+        click_on("Patients")
+      end
 
+      click_on("View")
+      expect(page).to have_text(@patient.full_name)
 
-    #   click_on("View")
-    #   expect(page).to have_text(@patient.full_name)
-
-    # end
+    end
   end
 end
--- spec/system/patients/new_patient_spec.rb
+++ spec/system/patients/new_patient_spec.rb
@@ -9,58 +9,79 @@ feature 'Create Patient spec' do
     FactoryBot.create(:lsg_body)
     FactoryBot.create(:ward)
     FactoryBot.create(:facility, kind: 'CHC')
-    @patient = FactoryBot.create(:patient)
+    FactoryBot.create(:patient)
     (1...5).each do
       FactoryBot.create(:volunteer)
     end
   end
 
-  context 'For a superuser', js: true do
-    before do
+  # context 'For a superuser', js: true do
+  #   before do
+  #     @user = FactoryBot.create(:user, role: User.roles[:superuser], verified: true)
+  #     login_as(@user)
+
+  #     within(find("div.desktopLayoutSidebar")) do
+  #       click_on("Enroll New Patients")
+  #     end
+
+  #     expect(page).to have_text("Create Patient")
+  #   end
+
+  #   scenario 'Creating a new patient' do
+  #     name = generate(:full_name)
+  #     dob = Faker::Date.in_date_period
+  #     gender = ["Male", "Female", "Other"].sample
+  #     phone = Faker::Number.number(digits: 10)
+  #     emergency = Faker::Number.number(digits: 10)
+  #     facility = Facility.last.name
+  #     route = Faker::Address.full_address
+  #     address = Faker::Address.full_address
+  #     notes = Faker::Lorem.paragraph
+  #     volunteer = User.where(role: "Volunteer").last
+  #     economic_status = ["Well Off", "Middle Class", "Poor", "Very Poor"].sample
+
+  #     fill_in "patient[full_name]", with: name
+  #     fill_in "patient[dob]", with: dob
+  #     select gender, from: "patient[gender]"
+  #     fill_in "patient[phone]", with: phone
+  #     fill_in "patient[emergency_phone_no]", with: emergency
+  #     select facility, from: "patient[facility_id]"
+  #     fill_in "patient[route]", with: route
+  #     fill_in "patient[address]", with: address
+  #     fill_in "patient[notes]", with: notes
+  #     # find("label[name='patient[volunteer[#{volunteer.id}]]']").click
+  #     select economic_status, from: "patient[economic_status]"
+
+  #     within(find("div.actions")) do
+  #       click_on("Submit")
+  #     end
+
+  #     expect(page).to have_text("Patients")
+  #     expect(page).to have_text(name)
+  #     expect(page).to have_text(dob)
+  #     expect(page).to have_text(phone)
+  #     expect(page).to have_text(address)
+  #   end
+  # end
+  context "Personal Details of Patient", js: true do
+    scenario "Edit Name in Personal Details" do
+      @id = Patient.last.id
       @user = FactoryBot.create(:user, role: User.roles[:superuser], verified: true)
       login_as(@user)
 
       within(find("div.desktopLayoutSidebar")) do
-        click_on("Enroll New Patients")
+        click_on("Patients")
       end
 
-      expect(page).to have_text("Create Patient")
-    end
-
-    scenario 'Creating a new patient' do
-      name = generate(:full_name)
-      dob = Faker::Date.in_date_period
-      gender = ["Male", "Female", "Other"].sample
-      phone = Faker::Number.number(digits: 10)
-      emergency = Faker::Number.number(digits: 10)
-      facility = Facility.last.name
-      route = Faker::Address.full_address
-      address = Faker::Address.full_address
-      notes = Faker::Lorem.paragraph
-      volunteer = User.where(role: "Volunteer").last
-      economic_status = ["Well Off", "Middle Class", "Poor", "Very Poor"].sample
-
-      fill_in "patient[full_name]", with: name
-      fill_in "patient[dob]", with: dob
-      select gender, from: "patient[gender]"
-      fill_in "patient[phone]", with: phone
-      fill_in "patient[emergency_phone_no]", with: emergency
-      select facility, from: "patient[facility_id]"
-      fill_in "patient[route]", with: route
-      fill_in "patient[address]", with: address
-      fill_in "patient[notes]", with: notes
-      # find("label[name='patient[volunteer[#{volunteer.id}]]']").click
-      select economic_status, from: "patient[economic_status]"
-
-      within(find("div.actions")) do
-        click_on("Submit")
+      within(find("tbody.patients_table")) do
+        click find_by_id(@id)
       end
 
-      expect(page).to have_text("Patients")
-      expect(page).to have_text(name)
-      expect(page).to have_text(dob)
-      expect(page).to have_text(phone)
-      expect(page).to have_text(address)
+      click_on('Edit Personal Details')
+
+      fill_in "patient[full_name]", with: "Sample Test"
+      click_on "Submit"
+      expect(page).to have_text("Sample Test")
     end
   end
 end
