8c095eef307048523b140ca5ab699eccecc0d802
Peeyush <peeyushgoyal154@gmail.com>
2021-11-15 23:20:06 +0530

    Revert Treatment Branch

--- db/seeds/development/users.seeds.rb
+++ db/seeds/development/users.seeds.rb
@@ -3,8 +3,8 @@ after "development:superuser" do
     User.create!(
       full_name: Faker::Name.name,
       role: User.roles.values.sample,
-      encrypted_password: "password",
-      password: "password",
+      encrypted_password: "password123",
+      password: "password123",
       email: Faker::Internet.email,
       phone: rand(10 ** 10),
     )
