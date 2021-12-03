966705d7db7450e94ee2a3ab2bb322f550a6b44b
Khavin Shankar <khavinshankar@gmail.com>
2021-12-03 11:04:30 +0530

    refactor (Agenda): removed puts in controller

--- app/controllers/agenda_controller.rb
+++ app/controllers/agenda_controller.rb
@@ -1,7 +1,7 @@
 class AgendaController < ApplicationController
   skip_before_action :verify_authenticity_token
+  
   def index
     @scheduled_patients = Visit.scheduled_patients
-    puts @scheduled_patients
   end
 end
